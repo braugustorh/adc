9f4cad69c3e35787a0d09f8b9aeeefde34e115f3
GSilver <geodmsgit@gsilver.com>
2025-03-10 19:36:54 -0600

    Corrección de f2f, Clima, y Schedule Include

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:app:verify-execution')
+        $schedule->command('app:verify-execution')
             ->everyMinute()
             ->appendOutputTo(storage_path('logs/laravel.log'));
        /* $schedule->command('campaign:update-status')
